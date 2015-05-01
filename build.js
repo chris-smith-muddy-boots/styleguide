6e81a2187cb227478a2c317bc19a15d916bdb861
greenlightstyleguide <chris.smith@muddyboots.com>
2015-05-01 16:57:25 +0100

    Added Google Custom Search

--- build.js
+++ build.js
@@ -104,5 +104,17 @@
         loadHTML('header.html', document.body);
         addIcons();
     });
+
+    // google custom search
+    (function () {
+        var cx = '008181550016362218113:epoeayq-jym';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+            '//cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+    })();
     
 })();
